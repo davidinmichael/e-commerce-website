82333dcb6e5030d77478a31e9fd594b97890bb3b
davidinmichael <davidinmichael@gmail.com>
2025-10-13 22:33:53 +0100

    Feat: Admin app added

---
+++ admin_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from account.forms import AccountForm
+from django.views import View
+
+class RegisterAdminView(View):
+    
+    def get(self, request):
+        pass
+    
+    def post(self, request):
+        pass
--- e_commerce_website/settings/base.py
+++ e_commerce_website/settings/base.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "core",
     "account",
+    "admin_app",
 ]
 
 MIDDLEWARE = [
